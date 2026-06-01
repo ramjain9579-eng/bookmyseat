c95590562a400693b9485aa3293a112f36749c8d
ramjain9579-eng <ramjain9579@gmail.com>
2026-06-01 16:27:06 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ WSGI config for bookmyseat project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
 application = get_wsgi_application()
+app = application
